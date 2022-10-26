1a5f1acd901ebc37ad77a126307b6f4275c57250
Paulo Costa <pjrc83@gmail.com>
2022-10-26 17:42:21 -0300

    Dados inválidos

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -6,19 +6,27 @@ import org.junit.jupiter.api.Test;
 import entities.Financing;
 
 public class FinancingTests {
-	
+
 	@Test
 	public void construcorShouldCreateObjectWhenValidData() {
-		
-		//arrange
-		
-		//action
+
+		// arrange
+
+		// action
 		Financing f = new Financing(100000.0, 2000.0, 80);
-		
+
 		// assert
-		
+
 		Assertions.assertEquals(100000.0, f.getTotalAmount());
 		Assertions.assertEquals(2000.0, f.getIncome());
 		Assertions.assertEquals(80, f.getMonths());
+	}
+	@Test
+	public void construcorShouldIThrowllegalArgumentExceptiontWhenValidData() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+			Financing f = new Financing(100000.0, 2000.0, 20);
+		});
+		
 	}
 }
