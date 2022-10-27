af61ccd1f3534cc69f2d28dd0ac3d1d19a2a2822
Paulo Costa <pjrc83@gmail.com>
2022-10-27 16:40:17 -0300

    entrey

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -89,4 +89,10 @@ public class FinancingTests {
 		});
 		
 	}
+	@Test
+	public void entreyShouldCalculateEntryCorrectly() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		Assertions.assertEquals(20000.0, f.entry());
+	}
 }
